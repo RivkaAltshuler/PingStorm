30ee6d8881560996a92e84f05dca0a4e0374af71
Rivka Altshuler <brandrivka@gmail.com>
2025-04-01 01:48:28 +0300

    fix loading the data

--- load_results.sh
+++ load_results.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+rm sorted_results
+cat "$results_file" | sort -k 2 -n >> sorted_results
 
-results_array=$(cat $results_file | sort -k 2 -n)
+while IFS= read -r line; do
+  results_array+=("$line")
+done < sorted_results
 
 
-# echo "$results_array"
 
-max=$(printf "%s\n" "${results_array[@]}" | head -n 1 | awk '{print $2}')
-min=$(printf "%s\n" "${results_array[@]}" | tail -n 1 | awk '{print $2}')
+last_index=$(( ${#results_array[@]} - 1 ))
+min=$(echo "${results_array[0]}" | awk '{print $2}')
+max=$(echo "${results_array[$last_index]}" | awk '{print $2}')
 
-# echo "$max $min"
+
+
+
 
 
 
