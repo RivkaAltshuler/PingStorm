edf9de0ebc3825e693b94ab6f3fbbf14440ff4b6
Rivka Altshuler <brandrivka@gmail.com>
2025-03-31 22:23:30 +0300

    ping to results file and basic log, happy path

--- PingStorm.sh
+++ PingStorm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 script_name=$(basename "$0")
-log_file="ping_results"
+log_file="pingstorm.log"
+results_file="results"
+
 
 # Check if a domain file argument is provided
 if [[ -z "$1" ]]; then
@@ -17,6 +19,9 @@ if [[ ! -f "$domains_file" ]]; then
   exit 1
 fi
 
+rm $results_file
+
+
 # Load domains from the file into an array
 declare -a domains
 while IFS= read -r domain; do
@@ -26,18 +31,27 @@ done < "$domains_file"
 # Loop through each domain
 for domain in "${domains[@]}"; do
   ping_output=$(ping -c 1 -W 1 "$domain")
-  latency_output=$(echo "$ping_output" | awk '/time=/{gsub(/time=/, ""); gsub(/ ms$/, ""); print $NF}')
+  
+  average_latency=$(echo "$ping_output" | awk '/rtt min\/avg\/max\/mdev/{split($4, a, "/"); print a[2]}')
+
   return_code=$?
 
   timestamp=$(date "+%Y-%m-%d %H:%M:%S")
-  echo ""  >> "$log_file"
+  
   echo -n "$timestamp " >> "$log_file"
   echo -n "$script_name " >> "$log_file"
   echo -n "$domain " >> "$log_file"
+  
+  
+  echo -n "$domain " >> "$results_file"
 
   if [[ $return_code -eq 0 ]]; then
-    echo -n "$latency_output" >> "$log_file"
+    echo -n "$average_latency" >> "$log_file"
+	echo -n "$average_latency" >> "$results_file"
   fi
+  
+  echo ""  >> "$results_file"
+  echo ""  >> "$log_file"
 
 done
 
